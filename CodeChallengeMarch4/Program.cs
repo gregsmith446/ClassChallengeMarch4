b2a0e2c25be2d42d329dd71ec59571139f78456e
Greg Smith <gregsmith446@gmail.com>
2019-03-04 18:37:36 -0500

    Add project files.

---
+++ CodeChallengeMarch4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeChallengeMarch4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
