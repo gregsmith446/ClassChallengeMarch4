a66d97b9b48740d59708d7b8aefbfefe66c5801e
Greg Smith <gregsmith446@gmail.com>
2019-03-04 21:05:11 -0500

    working verion

--- CodeChallengeMarch4/Program.cs
+++ CodeChallengeMarch4/Program.cs
@@ -8,9 +8,9 @@ namespace CodeChallengeMarch4
         {
             var dropTheBall = new BouncingBall();
 
-            // int result = dropTheBall.bouncingBall(3, 0.66, 1.5);
+            int result = dropTheBall.bouncingBall(3, 0.66, 1.5);
 
-            int result = dropTheBall.bouncingBall(3, 1, 1.5);
+            // int result = dropTheBall.bouncingBall(3, 1, 1.5);
 
             Console.WriteLine(result);
         }
@@ -19,32 +19,35 @@ namespace CodeChallengeMarch4
         {
             public int bouncingBall(double h, double bounce, double window)
             {
-                int total = 0;
-
                 // if any of these are true, exit the program with a -1
-                // this means the numbers are invalid
+                // this means the given numbers are in some way invalid
+                int total = -1;
+
                 if (h < 0)
                 {
                     Console.WriteLine("height invalid");
-                    return -1;
+                    return total;
                 }
                 else if (window >= h)
                 {
                     Console.WriteLine("window height invalid");
-                    return -1;
+                    return total;
                 }
-                else if (bounce <= 0)
+                else if (bounce <= 0 || bounce >= 1)
                 {
                     Console.WriteLine("bounce rate invalid");
-                    return -1;
+                    return total;
                 }
-                else if (bounce >= 1)
-                {
-                    Console.WriteLine("bounce rate invalid");
-                    return -1;
-                }
+                // count how many times the ball passed by window
+                // each time the height is > window, add 2 to totat
+                // first bounce will count as positive 1 since the total is initially set to -1
                 else
                 {
+                    while (h >= window)
+                    {
+                        h = h * bounce;
+                        total = total + 2;
+                    }
                     return total;
                 }
             }
