edec2f239dcbed48bac7bcdc977249e8247064ba
Greg Smith <gregsmith446@gmail.com>
2019-03-07 11:38:12 -0500

    refactored error checking + separated code by concerns

---
+++ CodeChallengeMarch4/BouncingBall.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class BouncingBall
+{
+    public int bouncingBall(double h, double bounce, double window)
+    {
+        // if any of these are true, exit the program with a -1
+        // this means the given numbers are in some way invalid
+        int total = -1;
+
+        if (h < 0 && window >= h && bounce <= 0 && bounce >= 1)
+        {
+            return total;
+        }
+        // count how many times the ball passed by window
+        // each time the height is > window, add 2 to total
+        // first bounce will count as positive 1 since the total is initially set to -1
+        else
+        {
+            while (h >= window)
+            {
+                h = h * bounce;
+                total = total + 2;
+            }
+            return total;
+        }
+    }
+}
--- CodeChallengeMarch4/Program.cs
+++ CodeChallengeMarch4/Program.cs
@@ -6,47 +6,13 @@ namespace CodeChallengeMarch4
     {
         static void Main(string[] args)
         {
-            var dropTheBall = new BouncingBall();
+            BouncingBall dropTheBall = new BouncingBall();
 
+            // test different cases here by changing inputs
             int result = dropTheBall.bouncingBall(3, 0.66, 1.5);
 
-            // int result = dropTheBall.bouncingBall(3, 1, 1.5);
+            // uncomment to see the result in the console
+            // Console.WriteLine(result);
         }
-             
-        public class BouncingBall
-        {
-            public int bouncingBall(double h, double bounce, double window)
-            {
-                // if any of these are true, exit the program with a -1
-                // this means the given numbers are in some way invalid
-                int total = -1;
-
-                if (h < 0)
-                {
-                    return total;
-                }
-                else if (window >= h)
-                {
-                    return total;
-                }
-                else if (bounce <= 0 || bounce >= 1)
-                {
-                    return total;
-                }
-                // count how many times the ball passed by window
-                // each time the height is > window, add 2 to totat
-                // first bounce will count as positive 1 since the total is initially set to -1
-                else
-                {
-                    while (h >= window)
-                    {
-                        h = h * bounce;
-                        total = total + 2;
-                    }
-                    return total;
-                }
-            }
-        }
-
     }
 }
